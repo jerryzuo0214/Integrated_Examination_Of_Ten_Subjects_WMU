75e6af87f4571f282ce0290d237bfa910927a3eb
左子懿 <jerryzuo0214@msn.cn>
2021-01-27 00:34:07 +0800

    增加帮助页面的访问入口，初次使用的访问

--- app/src/main/java/com/jerry/zuo/Integrated_Examination_Of_Ten_Subjects_WMU/fragment/SearchFragment.java
+++ app/src/main/java/com/jerry/zuo/Integrated_Examination_Of_Ten_Subjects_WMU/fragment/SearchFragment.java
@@ -1,12 +1,16 @@
 package com.jerry.zuo.Integrated_Examination_Of_Ten_Subjects_WMU.fragment;
 
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jerry.zuo.Integrated_Examination_Of_Ten_Subjects_WMU.R;
@@ -19,11 +23,21 @@ import com.jerry.zuo.Integrated_Examination_Of_Ten_Subjects_WMU.activity.SearchA
 public class SearchFragment extends Fragment {
     private View rootView;
     private TextView sv;
+    private ImageView help;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView=inflater.inflate(R.layout.fragment_search,container,false);
         this.rootView=rootView;
+        help=rootView.findViewById(R.id.helppage);;
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://jerryzuo0214.github.io/Integrated_Examination_Of_Ten_Subjects_WMU/");    //设置跳转的网站
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
         sv=rootView.findViewById(R.id.tv_sv);
         sv.setOnClickListener(new View.OnClickListener() {
             @Override
